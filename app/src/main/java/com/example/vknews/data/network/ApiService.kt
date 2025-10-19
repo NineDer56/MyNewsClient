beea9741a17354cdd01b9de0aabb8deadcf81ba1
Ilya <Ilya.boltyshev@mail.ru>
2025-10-19 12:14:58 +0300

    only russian posts

--- app/src/main/java/com/example/vknews/data/network/ApiService.kt
+++ app/src/main/java/com/example/vknews/data/network/ApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("latest?apikey=pub_9909b1f19ee047f29dd64cfa8c8bbf73")
+    @GET("latest?apikey=pub_9909b1f19ee047f29dd64cfa8c8bbf73&language=ru")
     suspend fun getLatestNews(
         @Query("page") page: String?
     ): NewsResponseDto
